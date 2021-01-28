1efebeecdf18962fa50bbc01247ba36956de8289
Caypoh23 <caypoh23@gmail.com>
2021-01-28 15:47:33 +0500

    feat: setup scene, valkyrie movement. beta object pool

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Interfaces;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour, IPooledObject
+{
+    [SerializeField] private Rigidbody2D valkirieRigidbody2D;
+    [SerializeField] private Transform SpawnPoint;
+    [SerializeField] private Tag valkirieTag;
+    private float movement = 5;
+    [SerializeField] private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        valkirieRigidbody2D.velocity = new Vector2( movement * speed, valkirieRigidbody2D.velocity.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        ObjectPooler.Instance.SpawnFromPool(valkirieTag, SpawnPoint.position, Quaternion.identity);
+    }
+
+    public void OnObjectSpawn()
+    {
+        Debug.Log("Spawned");
+    }
+}
---
+++ Assets/Scripts/ValkirieSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Interfaces;
+using UnityEngine;
+
+public class ValkirieSpawner : SingletonClass<ValkirieSpawner>, IPooledObject
+
+{
+    [SerializeField] private Tag valkirieTag;
+    [SerializeField] private Transform spawnPoint;
+    private ObjectPooler _objectPooler;
+    public override void Awake()
+    {
+        _objectPooler = ObjectPooler.Instance;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        ObjectPooler.Instance.SpawnFromPool(valkirieTag, spawnPoint.position, Quaternion.identity);
+    }
+    
+    public void OnObjectSpawn()
+    {
+        
+    }
+    
+}
